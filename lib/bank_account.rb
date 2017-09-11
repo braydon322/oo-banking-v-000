1af76ba23002d39aba73d928cdbad36f6fc752e7
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 23:49:04 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,11 @@
 class BankAccount
 
-  attr_accessor :balance
+  attr_accessor :balance, :status
   attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
-
   end
 
 end
