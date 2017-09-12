f8576690395208c1b83c7472b74bfbc754868b1d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 00:21:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,6 @@ class Transfer
     sender.balance = sender.balance + amount
     receiver.balance = receiver.balance - amount
     @status = "reversed"
-    binding.pry
   end
 
   def execute_transaction
