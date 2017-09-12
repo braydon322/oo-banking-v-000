686d67a87b6822fcabfef5088ec838a3f23e75c9
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-12 00:14:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
     if valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
-      status = "complete"
+      @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
